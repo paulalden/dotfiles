47943be47d667aabcaf8c50ca1a7b9ec7fab9da7
Paul Danelli <prdanelli@gmail.com>
2025-11-15 16:23:16 +0000

    Tweaks

--- zsh/config/exports.sh
+++ zsh/config/exports.sh
@@ -13,7 +13,7 @@ export PATH=/opt/homebrew/opt/python@3.13:$PATH
 export PATH=/opt/homebrew/sbin:$PATH # Recommended by brew doctor
 export PATH=/opt/homebrew/bin:$PATH  # Brew is first as everything else uses that
 export PATH=/opt/homebrew/opt/rustup/bin:$PATH
-export PATH="/opt/homebrew/opt/openssl@3.5/bin:$PATH"
+export PATH=/opt/homebrew/opt/openssl@3.5/bin:$PATH
 export PATH=/opt/homebrew/opt/openssl@3:$PATH
 
 export LIBRARY_PATH=$LIBRARY_PATH:/opt/homebrew/opt/zstd/lib/ # Fix for MySQL2 gem not compiling
@@ -23,14 +23,14 @@ export CXX=/usr/bin/clang++
 export LANG=en_GB.UTF-8
 
 # Olio API specific
-export RAILS_ENV="development"
+export RAILS_ENV=development
 
 # Disable homebrew post-install messages
 export HOMEBREW_NO_ENV_HINTS=1
 
 # Preferred programs
-export EDITOR="nvim"
-export TERMINAL="kitty"
+export EDITOR=nvim
+export TERMINAL=kitty
 export MANPAGER='nvim +Man!'
 
 # Mobile app
