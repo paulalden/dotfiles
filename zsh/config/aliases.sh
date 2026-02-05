8f453215a488fa8b4433f21991a75a409a1cbe6d
Paul Danelli <prdanelli@gmail.com>
2026-02-05 11:37:03 +0000

    Remove olio aliass

--- zsh/config/aliases.sh
+++ zsh/config/aliases.sh
@@ -33,22 +33,6 @@ alias ta="t a -t"
 alias tls="t ls"
 alias tn="t new -t"
 
-# Olio
-alias olio:api:console:production="FILTER=admin bundle exec cap production rails:console"
-alias olio:api:console:staging="bundle exec cap staging rails:console"
-
-alias olio:storage="zsh ~/Dev/storage.sh"
-alias olio:stop_storage="docker stop redis-dev mysql-dev"
-function olio_ips() {
-  aws ec2 describe-instances \
-    --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PrivateIpAddress:PrivateIpAddress,Name:Tags[?Key=='Name']|[0].Value}" \
-    --output table | fzf
-}
-alias olio:ips="olio_ips"
-# function olio_console() {
-#   FILTER=admin bundle exec cap $1 rails:console
-# }
-
 # Yabai
 alias yabai:reload="yabai --restart-service"
 alias yabai:restart="yabai --restart-service"
@@ -67,12 +51,15 @@ alias skhd:reload="skhd --restart-service"
 
 # Brew
 # alias brew:upgrade:all="brew upgrade; sh ~/Personal/Repos/dotfiles/zsh/user/install_neovim_head.sh; nvim --headless '+Lazy! sync' +qa; yabai:stop; yabai:start;"
-alias brew:upgrade:all="brew upgrade; nvim --headless '+Lazy! sync' +qa > /dev/null; yabai:stop; yabai:start;"
+alias brew:upgrade:all="brew upgrade; nvim --headless '+Lazy! sync' +qa > /dev/null; yabai:stop; yabai:start; ~/.config/sketchybar/scripts/update_homebrew_cache.sh"
+alias brew:upgrade="brew upgrade && ~/.config/sketchybar/scripts/update_homebrew_cache.sh"
 alias brew:bundle="brew bundle --file ~/.Brewfile"
 
 # Docker Compose
 alias dc="docker-compose"
 alias dcr="docker-compose run --rm"
+alias dce="docker-compose exec"
+
 # DO NOT use ctrl+c or it will exit the container, instead use ctrl+q+p
 function docker-attach() {
   docker attach $(docker-compose ps -q $1)
@@ -81,11 +68,4 @@ function docker-attach() {
 # Rails
 alias rails:routes="dcr rails routes | fzf -e"
 
-# Ollama
-alias ai:install="docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama"
-alias ai:start="docker start ollama"
-alias ai:stop="docker stop ollama"
-alias ai="docker exec -it ollama ollama run mistral:latest"
-alias ai:code="docker exec -it ollama ollama run mistral:ruby-mentor"
-alias ai:ls="docker exec -it ollama ollama ls"
-alias ai:rm="docker exec -it ollama ollama rm"
+alias speed-test="cloudflare-speed-cli"
