ffc67d91cf957d87177634bd23cab3af4517ed67
Paul Danelli <prdanelli@gmail.com>
2026-02-25 16:08:26 +0000

    Remove dup keymaps

--- neovim/lua/config/keymaps.lua
+++ neovim/lua/config/keymaps.lua
@@ -93,8 +93,6 @@ map("n", "<leader>yf", "<cmd>let @+ = expand('%:p')<cr>", { desc = "Full Path", 
 -- Navigation between buffers
 map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 
 -- Switch to other buffer
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
