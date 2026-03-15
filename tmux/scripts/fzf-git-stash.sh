f9a470f7193ef7f85d8299990f2f1ad17315679b
Paul Danelli <prdanelli@gmail.com>
2026-03-15 13:02:06 +0000

    Combine all keymaps

--- tmux/scripts/fzf-git-stash.sh
+++ tmux/scripts/fzf-git-stash.sh
@@ -5,6 +5,7 @@
 git stash list | \
   fzf --no-tmux --header 'Enter: apply, Ctrl-D: drop' \
       --preview 'git stash show -p --color=always $(echo {} | cut -d: -f1)' \
+      --preview-window=right,60% \
       --bind 'ctrl-d:become(git stash drop $(echo {} | cut -d: -f1))' \
       --exit-0 | \
   cut -d: -f1 | xargs -r git stash apply
--- tmux/scripts/tmux-sessionizer.sh
+++ tmux/scripts/tmux-sessionizer.sh
@@ -7,7 +7,7 @@ if [[ $# -eq 1 ]]; then
 else
   # if no directory is passed in, use fzf to select one
   # NOTE: change the directories to search in the find command as you wish
-  selected=$(FZF_TMUX=1 find ~/Dev ~ ~/Personal/repos/dotfiles -mindepth 1 -maxdepth 1 -type d | fzf)
+  selected=$(find ~/Dev ~ ~/Personal/repos/dotfiles -mindepth 1 -maxdepth 1 -type d 2>/dev/null | fzf --no-tmux)
 fi
 
 # exit if no directory is selected from fzf
