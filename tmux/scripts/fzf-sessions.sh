720e1b5cbfddbc86ee1a741f724f52ff0e353afc
Paul Danelli <prdanelli@gmail.com>
2026-03-19 13:21:11 +0000

    Add session rename

--- tmux/scripts/fzf-sessions.sh
+++ tmux/scripts/fzf-sessions.sh
@@ -8,12 +8,18 @@ reload_cmd="tmux list-sessions -F '#S (#{session_windows} windows) #{?session_at
 
 eval "$reload_cmd" \
   | fzf --no-tmux +m --reverse --exit-0 --no-preview \
-    --header "Enter: switch | Ctrl-X: kill | Ctrl-A: new" \
+    --header "Enter: switch | Ctrl-X: kill | Ctrl-A: new | Ctrl-R: rename" \
     --bind "ctrl-x:execute-silent(
       session=\$(echo {} | awk '{print \$1}');
       [ \"\$session\" != \"$current_session\" ] && tmux kill-session -t \"\$session\"
     )+reload($reload_cmd)" \
     --bind "ctrl-a:execute-silent(tmux new-session -d)+reload($reload_cmd)" \
+    --bind "ctrl-r:execute(
+      session=\$(echo {} | awk '{print \$1}');
+      printf 'Rename \"%s\" to: ' \"\$session\";
+      read name;
+      [ -n \"\$name\" ] && tmux rename-session -t \"\$session\" \"\$name\"
+    )+reload($reload_cmd)" \
     --bind "enter:become(
       session=\$(echo {} | awk '{print \$1}');
       tmux switch-client -t \"\$session\"
