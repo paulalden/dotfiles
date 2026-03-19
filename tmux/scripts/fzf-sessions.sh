d308998fc743b91a414a2e1493e3aa260517a232
Paul Danelli <prdanelli@gmail.com>
2026-03-19 13:20:00 +0000

    Update session shortcuts

--- tmux/scripts/fzf-sessions.sh
+++ tmux/scripts/fzf-sessions.sh
@@ -8,12 +8,12 @@ reload_cmd="tmux list-sessions -F '#S (#{session_windows} windows) #{?session_at
 
 eval "$reload_cmd" \
   | fzf --no-tmux +m --reverse --exit-0 --no-preview \
-    --header "Enter: switch | Ctrl-X: kill | Ctrl-N: new" \
+    --header "Enter: switch | Ctrl-X: kill | Ctrl-A: new" \
     --bind "ctrl-x:execute-silent(
       session=\$(echo {} | awk '{print \$1}');
       [ \"\$session\" != \"$current_session\" ] && tmux kill-session -t \"\$session\"
     )+reload($reload_cmd)" \
-    --bind "ctrl-n:execute-silent(tmux new-session -d)+reload($reload_cmd)" \
+    --bind "ctrl-a:execute-silent(tmux new-session -d)+reload($reload_cmd)" \
     --bind "enter:become(
       session=\$(echo {} | awk '{print \$1}');
       tmux switch-client -t \"\$session\"
