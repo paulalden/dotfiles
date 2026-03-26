dc87425e375d27331edc2d78b3a76225f05fc02b
Tom Ward <twarddev@gmail.com>
2026-03-26 14:44:00 -0700

    Remove Ruby-specific configs

--- zsh/config/aliases.sh
+++ zsh/config/aliases.sh
@@ -16,16 +16,6 @@ alias vimdiff='nvim -d'
 alias vim="nvim"
 alias nvim-kickstart='NVIM_APPNAME="nvim-kickstart" nvim'
 
-# Rails / Ruby
-alias rspec="bundle exec rspec"
-alias be="bundle exec"
-alias rc="bundle exec rails console"
-alias rs="bundle exec rails server"
-alias rdm="bundle exec rails db:migrate"
-alias rdr="bundle exec rails db:rollback"
-alias rg:m="bundle exec rails generate migration"
-alias ba="bundle audit check --update"
-
 # ZSH
 alias zsh:reload='source $ZDOTDIR/.zshrc'
 alias zsh:edit="nvim $ZDOTDIR/.zshrc"
@@ -70,9 +60,6 @@ alias dce="docker-compose exec"
 function docker-attach() {
   docker attach $(docker-compose ps -q $1)
 }
-
-# Rails
-alias rails:routes="dcr rails routes | fzf -e"
 
 alias speed-test="cloudflare-speed-cli"
 
